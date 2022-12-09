0197ee671e832ecd50b9c435d81459e51f27c45c
Cheree <71246795+Chere3@users.noreply.github.com>
2022-12-09 12:51:02 -0600

    Cambiados testings

--- tests/test.js
+++ tests/test.js
@@ -4,44 +4,4 @@ const Commander = new NamiConsole.Commander();
 
 console = new NamiConsole.Console();
 
-Commander.setPrefix(
-    console.formatString("Input", "$ "));
-
-Commander.registerCommand("test", (args) => {
-    
-    console.log("Arguments: " + args);
-    
-    console.log("Hello asdasdasdasd", "world", "here", "my", "code", "example")
-});
-
-Commander.registerCommand("eval", (args) => {
-    let expression = args.join(" ");
-    try {
-        let evaluated = eval(expression);
-        if (evaluated != null)
-            
-        console.log("Evaled code result: " + evaluated);
-    } catch (e) {
-        // @ts-ignore
-        
-        console.error("Failed to eval code: " + e.message);
-    }
-})
-
-Commander.onUnknownCommand((cmd) => {
-    
-    console.error("Invalid Command: " + cmd);
-})
-
-Commander.fetch();
-
-let x = "aa";
-
-function greet(name) {
-    
-    console.log("Hello " + name);
-}
-
-function plus() {
-    return 5 + 10;
-}
+console.log("a")
