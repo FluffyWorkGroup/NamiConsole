c451cbfdab3695ba356af19e2df10aae3adcf287
Cheree <71246795+Chere3@users.noreply.github.com>
2022-12-11 11:08:08 -0600

    Quitado test

--- tests/test.ts
+++ tests/test.ts
@@ -1,8 +1,2 @@
-import { Console } from "nami-console"
-
-const log = new Console();
-
-log.log("Esto es un log normal."); log.error("Esto es un error."); log.warn("Esto es un warning.");
-log.info(`Esto es un info.`); log.debugLog("Esto es un debug.");
 
 
