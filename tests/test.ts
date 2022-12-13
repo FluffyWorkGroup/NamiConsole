d4c56c8665e8274e7d843cd264c788f7b0439cda
Cheree <71246795+Chere3@users.noreply.github.com>
2022-12-12 19:19:16 -0600

    Versión de testing final

--- tests/test.ts
+++ tests/test.ts
@@ -1,8 +1,35 @@
-import { Color, Console, Formats} from "nami-console";
+// @ts-ignore
+import { Formats, Console, Color } from "../src";
 
 
 const namiConsole = new Console({
     use_colors: true
 });
 
+
+// types of console.
 namiConsole.log("Hello World!");
+namiConsole.info("Hello World!");
+namiConsole.debugLog("Hello World!");
+namiConsole.error("Hello World!");
+namiConsole.warn("Hello World!");
+namiConsole.debug("Hello World!");
+
+// formats
+namiConsole.debug(Formats.Bold + "Hello World!" + Formats.Reset);
+namiConsole.debug(Formats.Underline + "Hello World!" + Formats.Reset);
+namiConsole.debug(Formats.Hidden + "Hello World!" + Formats.Reset);
+namiConsole.debug(Formats.Italic + "Hello World!" + Formats.Reset);
+namiConsole.debug(Formats.Reverse + "Hello World!" + Formats.Reset);
+namiConsole.debug(Formats.Strike + "Hello World!" + Formats.Reset);
+
+// colors
+namiConsole.debug(Color.Red + "Hello world" + Formats.Reset);
+namiConsole.debug(Color.Green + "Hello world" + Formats.Reset);
+namiConsole.debug(Color.Yellow + "Hello world" + Formats.Reset);
+namiConsole.debug(Color.Blue + "Hello world" + Formats.Reset);
+namiConsole.debug(Color.DarkAqua + "Hello world" + Formats.Reset);
+namiConsole.debug(Color.Gray + "Hello world" + Formats.Reset);
+namiConsole.debug(Color.White + "Hello world" + Formats.Reset);
+namiConsole.debug(Color.Gray + "Hello world" + Formats.Reset);
+namiConsole.debug(Color.DarkGray + "Hello world" + Formats.Reset);
