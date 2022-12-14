09f81e95d4d03ecef17b43750bc279b85770e7c0
Cheree <71246795+Chere3@users.noreply.github.com>
2022-12-14 01:15:26 -0600

    Corregido error de tipeado

--- tests/test.ts
+++ tests/test.ts
@@ -1,11 +1,9 @@
 // @ts-ignore
-import { Formats, Console, Color } from "../src";
-
+import { Formats, Console, Colors } from "../src/index.js"
 
 const namiConsole = new Console({
     use_colors: true
 });
-
 
 // types of console.
 namiConsole.log("Hello World!");
@@ -24,12 +22,10 @@ namiConsole.debug(Formats.Reverse + "Hello World!" + Formats.Reset);
 namiConsole.debug(Formats.Strike + "Hello World!" + Formats.Reset);
 
 // colors
-namiConsole.debug(Color.Red + "Hello world" + Formats.Reset);
-namiConsole.debug(Color.Green + "Hello world" + Formats.Reset);
-namiConsole.debug(Color.Yellow + "Hello world" + Formats.Reset);
-namiConsole.debug(Color.Blue + "Hello world" + Formats.Reset);
-namiConsole.debug(Color.DarkAqua + "Hello world" + Formats.Reset);
-namiConsole.debug(Color.Gray + "Hello world" + Formats.Reset);
-namiConsole.debug(Color.White + "Hello world" + Formats.Reset);
-namiConsole.debug(Color.Gray + "Hello world" + Formats.Reset);
-namiConsole.debug(Color.DarkGray + "Hello world" + Formats.Reset);
+namiConsole.debug(Colors.Yellow + "Hello world" + Formats.Reset);
+namiConsole.debug(Colors.Blue + "Hello world" + Formats.Reset);
+namiConsole.debug(Colors.DarkAqua + "Hello world" + Formats.Reset);
+namiConsole.debug(Colors.Gray + "Hello world" + Formats.Reset);
+namiConsole.debug(Colors.White + "Hello world" + Formats.Reset);
+namiConsole.debug(Colors.Gray + "Hello world" + Formats.Reset);
+namiConsole.debug(Colors.DarkGray + "Hello world" + Formats.Reset);
