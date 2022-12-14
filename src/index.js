6ba201ed7f6af34200fbae4de2ffeb405cc2320c
Cheree <71246795+Chere3@users.noreply.github.com>
2022-12-14 01:08:17 -0600

    Arreglado error de tipeado.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function NamiConsole(config) {
 
 NamiConsole.Console = require('./constructors/console');
 NamiConsole.Commander = require('./constructors/commander');
-NamiConsole.Color = require('./enumerators/Color');
+NamiConsole.Colors = require('./enumerators/Color');
 NamiConsole.Formats = require('./enumerators/Formats');
 
 module.exports = NamiConsole;
